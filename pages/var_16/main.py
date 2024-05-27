12b240ff32c440013f2e1c29407210b33b84b551
Наталья <nata.tarasevich89@gmail.com>
2024-05-27 14:34:17 +0300

    git_push

--- pages/var_16/main.py
+++ pages/var_16/main.py
@@ -1,4 +1,27 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+
+
+def count_survivors(filename, price):
+    first_class_survived = 0
+    second_class_survived = 0
+    third_class_survived = 0
+
+    with open(filename) as file:
+        next(file)
+        for line in file:
+            parts = line.strip().split(',')
+            if not price[0] <= float(parts[10]) <= price[1]:
+                continue
+            if parts[5] == 'female' and parts[1] == '1':
+                if parts[2] == '1':
+                    first_class_survived += 1
+                elif parts[2] == '2':
+                    second_class_survived += 1
+                elif parts[2] == '3':
+                    third_class_survived += 1
+
+    return [first_class_survived, second_class_survived, third_class_survived]
 
 
 def var16() -> None:
@@ -9,4 +32,17 @@ def var16() -> None:
         :return: None
         """
     data_path = 'src/data/data.csv'
-    # Ваш код будет ниже
+
+    st.header('Данные пассажиров Титаника')
+    st.write('Просмотр данных количества выживших женщин по каждому классу обслуживания, с диапазоном платы за проезд')
+    price = st.slider('Диапазон платы за проезд в $ 1000', 0, 1000, (0, 1000))
+
+    classes = ['Первый класс', 'Второй класс', 'Третий класс']
+    survivors = count_survivors(data_path, price)
+    fig, ax = plt.subplots()
+    ax.bar(classes, survivors)
+    ax.set_xlabel('Класс')
+    ax.set_ylabel('Количество выживших женщин')
+    ax.set_title('Количество выживших женщин по классам')
+
+    st.pyplot(fig)
