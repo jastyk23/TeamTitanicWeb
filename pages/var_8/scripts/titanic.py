cd06fc92d397fbe30bc7b53f930c8be4d513840f
jastyk23 <jesjusa@gmail.com>
2024-05-30 11:25:17 +0300

    Сделал класс более универсальным, + обработка ошибки

--- pages/var_8/scripts/titanic.py
+++ pages/var_8/scripts/titanic.py
@@ -2,7 +2,7 @@ import csv
 
 
 class Titanic:
-    def __init__(self, path_name: str):
+    def __init__(self, path_name: str = ''):
         self.path_name = path_name
         self._class_sex_data = {}
         self._sex_class_data = {}
@@ -14,13 +14,19 @@ class Titanic:
     def get_sex(self) -> dict:
         return self._sex_class_data
 
+    def get_data_from_list(self, list_data: list) -> None:
+        list_data = iter(list_data)
+        next(list_data)
+        self.__parse_file(list_data)
+
     def _parse_sex_data(self, row: list) -> None:
-        if not row[4] in self._class_sex_data:
+        if not row[4] in self._sex_class_data:
             self._sex_class_data[row[4]] = {
                 '1': 0,
                 '2': 0,
                 '3': 0,
             }
+        # print(self._sex_class_data)
         self._sex_class_data[row[4]][row[2]] += 1
 
     def _parse_class_data(self, row: list) -> None:
@@ -31,11 +37,22 @@ class Titanic:
             }
         self._class_sex_data[row[2]][row[4]] += 1
 
+    def __parse_file(self, file) -> None:
+        for row in file:
+            if type(row) is str:
+                row = row.split(',')
+            self._parse_class_data(row)
+            self._parse_sex_data(row)
+        self._class_sex_data = dict(sorted(self._class_sex_data.items()))
+
     def __get_parch_data_dict(self) -> None:
-        with open(self.path_name, 'r') as f:
-            next(f)
-            file = csv.reader(f, delimiter=',')
-            for row in iter(file):
-                self._parse_class_data(row)
-                self._parse_sex_data(row)
-            self._class_sex_data = dict(sorted(self._class_sex_data.items()))
+        try:
+            with open(self.path_name, 'r') as f:
+                file = csv.reader(f, delimiter=',')
+                next(file)
+                file = iter(file)
+                self.__parse_file(file)
+                f.close()
+
+        except FileNotFoundError:
+            return None
