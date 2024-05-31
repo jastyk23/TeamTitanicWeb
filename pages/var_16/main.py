8626918c533c0faf61fd2e31234cafdd95fe799a
Наталья <nata.tarasevich89@gmail.com>
2024-05-31 14:18:38 +0300

    Поменяла условия в фун.+ Добавила тесты git_push

--- pages/var_16/main.py
+++ pages/var_16/main.py
@@ -2,25 +2,26 @@ import streamlit as st
 import matplotlib.pyplot as plt
 
 
-def count_survivors(filename, price):
+def count_survivors(file, price):
     first_class_survived = 0
     second_class_survived = 0
     third_class_survived = 0
 
-    with open(filename) as file:
+    if type(file) is list:
+        file = file[1:]
+    else:
         next(file)
-        for line in file:
-            parts = line.strip().split(',')
-            if not price[0] <= float(parts[10]) <= price[1]:
-                continue
-            if parts[5] == 'female' and parts[1] == '1':
-                if parts[2] == '1':
-                    first_class_survived += 1
-                elif parts[2] == '2':
-                    second_class_survived += 1
-                elif parts[2] == '3':
-                    third_class_survived += 1
-
+    for line in file:
+        parts = line.strip().split(',')
+        if not price[0] <= float(parts[10]) <= price[1]:
+            continue
+        if parts[5] == 'female' and parts[1] == '1':
+            if parts[2] == '1':
+                first_class_survived += 1
+            elif parts[2] == '2':
+                second_class_survived += 1
+            elif parts[2] == '3':
+                third_class_survived += 1
     return [first_class_survived, second_class_survived, third_class_survived]
 
 
@@ -38,7 +39,8 @@ def var16() -> None:
     price = st.slider('Диапазон платы за проезд в $ 1000', 0, 1000, (0, 1000))
 
     classes = ['Первый класс', 'Второй класс', 'Третий класс']
-    survivors = count_survivors(data_path, price)
+    with open(data_path) as file:
+        survivors = count_survivors(file, price)
     fig, ax = plt.subplots()
     ax.bar(classes, survivors)
     ax.set_xlabel('Класс')
--- tests/var_16.py
+++ tests/var_16.py
@@ -1,5 +1,54 @@
 from pages.var_16.main import *
 
+def test_survivors_female_in_all_classes():
+    test_data = [
+        'PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked',
+        '1,1,1,0,0,female,0,0,0,0,71.23,0,0',
+        '2,1,2,0,0,male,0,0,0,0,8.13,0,0',
+        '3,1,2,0,0,female,0,0,0,0,24.1000,0,0',
+        '4,1,1,0,0,male,0,0,0,0,40.0500,0,0',
+        '5,1,3,0,0,female,0,0,0,0,14.9250,0,0',
+        '6,1,2,0,0,male,0,0,0,0,53.35,0,0',
+        '7,1,2,0,0,female,0,0,0,0,53.10,0,0',
+        '8,1,2,0,0,male,0,0,0,0,15.412224,0,0',
+        '9,1,1,0,0,male,0,0,0,0,33.924250,0,0',
+        '10,1,2,0,0,female,0,0,0,0,52.142,0,0',
+    ]
+    price = (0, 100)
+    assert count_survivors(test_data, price) == [1, 3, 1]
 
-def test():
-    ...
+
+def test_survivors_female_expensive_tickets_only():
+    test_data = [
+        'PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked',
+        '1,1,1,0,0,female,0,0,0,0,421.23,0,0',
+        '2,1,2,0,0,male,0,0,0,0,8.13,0,0',
+        '3,1,2,0,0,female,0,0,0,0,24.1000,0,0',
+        '4,1,1,0,0,female,0,0,0,0,412.0500,0,0',
+        '5,1,3,0,0,female,0,0,0,0,14.9250,0,0',
+        '6,1,2,0,0,male,0,0,0,0,53.35,0,0',
+        '7,1,2,0,0,female,0,0,0,0,122.10,0,0',
+        '8,1,2,0,0,male,0,0,0,0,105.424,0,0',
+        '9,1,1,0,0,male,0,0,0,0,33.924250,0,0',
+        '10,1,2,0,0,female,0,0,0,0,52.142,0,0',
+    ]
+    price = (100, 1000)
+    assert count_survivors(test_data, price) == [2, 1, 0]
+
+
+def test_not_survived_female():
+    test_data = [
+        'PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked',
+        '1,0,1,0,0,female,0,0,0,0,421.23,0,0',
+        '2,1,2,0,0,male,0,0,0,0,8.13,0,0',
+        '3,1,2,0,0,female,0,0,0,0,24.1000,0,0',
+        '4,0,1,0,0,female,0,0,0,0,412.0500,0,0',
+        '5,1,3,0,0,female,0,0,0,0,14.9250,0,0',
+        '6,1,2,0,0,male,0,0,0,0,53.35,0,0',
+        '7,0,2,0,0,female,0,0,0,0,122.10,0,0',
+        '8,0,3,0,0,male,0,0,0,0,105.424,0,0',
+        '9,1,1,0,0,male,0,0,0,0,33.924250,0,0',
+        '10,0,2,0,0,female,0,0,0,0,52.142,0,0',
+    ]
+    price = (100, 1000)
+    assert count_survivors(test_data, price) == [0, 0, 0]
